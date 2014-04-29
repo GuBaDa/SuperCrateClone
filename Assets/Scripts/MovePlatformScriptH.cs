0ebfc1e9dd930d3a8f2973125a26e115f0e09757
SpidersBro <barend.poot@gmail.com>
2014-04-29 19:19:10 +0200

    Added PlayerScript
    Can move on platforms via parenting, but this causes bugs. I’m working
    on something else, but its not done yet.

---
+++ Assets/Scripts/MovePlatformScriptH.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlatformScriptH : MonoBehaviour {
+	
+	private Vector3 startPosition;
+	private bool goLeft;
+	
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+		goLeft = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < startPosition.x - .3)
+			goLeft = false;
+		if (transform.position.x > startPosition.x + .3) 
+			goLeft = true;
+
+		if (goLeft) {
+			transform.Translate (Vector3.left * Time.deltaTime, Space.World);
+		} else {
+			transform.Translate (Vector3.right * Time.deltaTime, Space.World);
+		}
+
+
+
+
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	private Transform activePlatform;
+	private Vector3 moveDistance;
+
+	private bool grounded;
+	private bool jumped;
+	private float tempSpeed;
+	private float jumpHeight = 5;
+	private float doubleJumpHeight = 5;
+	public float MaxSpeed = 2;
+	public bool doubleJumpOn;
+	
+	/// Start this instance.
+	void Start () {
+		grounded = false;
+		jumped = false;
+	}
+	
+
+	
+	// FixedUpdate is called on fixed Times, use this for physics movements.
+	void FixedUpdate () {
+		move ();
+		OnDeath ();
+	}
+
+	// Update is called on each frame, this way the character immediatly reacts to jumps.
+	void Update(){
+
+		if (activePlatform == null) {
+			transform.parent = null;
+
+		}
+		doubleJump ();
+		jump ();
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision2D){	
+		if (collision2D.gameObject.name =="Ground" || collision2D.gameObject.name =="Platform" ) {
+			//Debug.Log("Hit ground");
+			transform.parent = null;
+			grounded = true;
+			if(collision2D.gameObject.name =="Platform") {
+				Debug.Log("Hit Platform");
+				activePlatform = collision2D.collider.transform;
+				transform.parent = activePlatform;
+
+			}
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D collision2D){
+		Debug.Log("Exit Platform");
+		activePlatform = null;
+	}
+
+
+	/// Move, Jump and DoubleJump //////
+	/// 
+	/// 
+	void move(){
+		if (Input.GetButton  ("Horizontal")) {
+			tempSpeed = Input.GetAxisRaw ("Horizontal") * MaxSpeed;
+			rigidbody2D.velocity = new Vector2 (tempSpeed, rigidbody2D.velocity.y);
+		}
+	}
+	void jump(){
+		if (Input.GetButtonDown  ("Jump") && grounded) {
+			rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, jumpHeight);
+			grounded = false;
+			jumped = true;
+		}
+	}
+	void doubleJump(){
+		if (doubleJumpOn) {
+			if(Input.GetButtonDown("Jump") && jumped ){
+				rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, doubleJumpHeight);
+				jumped = false;
+			}
+		}
+	}
+	
+	/// Misc functions, OnDeath, LoseHP(int), etc.. 
+	/// 
+	/// 
+	void OnDeath(){
+		if (transform.position.y < -10) {
+			Application.LoadLevel(Application.loadedLevel);
+			grounded = false;
+		
+		}
+	}
+	
+}
