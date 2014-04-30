317b67925484d3df2b1ae9758e713339772340cd
guusmost <guusmostart@gmail.com>
2014-04-30 22:01:12 +0200

    2d toolkit light test with controller

---
+++ Assets/testScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testScript : MonoBehaviour {
+
+	public float force;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 vect = new Vector2 (Input.GetAxisRaw("Horizontal") * force,Input.GetAxisRaw("Vertical") * force);
+		if (vect.x != 0 || vect.y !=0){
+			rigidbody2D.velocity = vect;
+
+     	}
+	}
+}
