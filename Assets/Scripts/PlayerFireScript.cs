c16349b9976bc54c445b983a7f7caaa77189fc17
davidalpha <david.alpha@gmail.com>
2014-05-13 18:51:16 +0200

    Jungle v0.3

---
+++ Assets/Scripts/PlayerFireScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFireScript : MonoBehaviour {
+
+	public GameObject projectilePrefab;
+	public float attackSpeed;
+	private float coolDown;
+	private AimScriptChild aimSight;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton(0) && Time.time > coolDown) {
+			Fire();
+		}
+	}
+
+
+	void Fire()
+	{
+		//Transform.position should be the position of the gun
+
+		Vector3 pos = Input.mousePosition;
+		pos.z = transform.position.z - Camera.main.transform.position.z;
+		pos = Camera.main.ScreenToWorldPoint (pos);
+
+		Quaternion q = Quaternion.FromToRotation(Vector3.up, pos-transform.position);
+		GameObject pPrefab = (GameObject) Instantiate (projectilePrefab, transform.FindChild("AimSight").transform.position, q);
+
+		pPrefab.rigidbody2D.AddForce (pPrefab.transform.up*1000);
+
+		coolDown = Time.time + attackSpeed;
+	}
+}
