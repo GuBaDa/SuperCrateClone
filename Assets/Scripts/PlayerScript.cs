253551f31ae899b86de59c634833f8db4f0f4808
SpidersBro <barendez@gmail.com>
2014-05-15 23:37:36 +0200

    Improved playerscript
    Still some buggy behavior sometimes when the character jumps up against
    walls. But no more sticking to walls/stuck on corners/not being able to
    jump when you should.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,6 +8,7 @@ public class PlayerScript : MonoBehaviour {
 	[HideInInspector] // Hides var below from inspector
 	public bool jumped;
 
+	private bool wallLeft, wallRight;
 	private Transform activePlatform;
 	private Vector3 tempScale;
 	public float jumpHeight;
@@ -19,6 +20,8 @@ public class PlayerScript : MonoBehaviour {
 	void Start () {
 		grounded = false;
 		jumped = false;
+		wallLeft = false;
+		wallRight = false;
 		doubleJumpHeight = jumpHeight * .75f;
 		tempScale = transform.localScale;
 	}
@@ -26,6 +29,8 @@ public class PlayerScript : MonoBehaviour {
 	
 	// FixedUpdate is called on fixed Times, use this for physics movements.
 	void FixedUpdate () {
+
+
 		move ();
 		OnDeath ();
 	}
@@ -33,12 +38,21 @@ public class PlayerScript : MonoBehaviour {
 	// Update is called on each frame, this way the character immediately reacts to jumps.
 	void Update(){
 		// Get all horizontal velocity properties from the object you're touching.
+
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector3.down, 1f);
+		if (hit.collider != null ) {
+			wallLeft = false;
+			wallRight = false;
+			grounded = true;
+		}
+
 		if (activePlatform != null) {
 			Vector2 tempVelocity;
 			tempVelocity.x = activePlatform.rigidbody2D.velocity.x;
 			tempVelocity.y = transform.rigidbody2D.velocity.y;
 			transform.rigidbody2D.velocity = tempVelocity;
 		}
+
 	
 
 
@@ -58,21 +72,38 @@ public class PlayerScript : MonoBehaviour {
 
 	// If the player hits something, he is grounded and interacts with this object.
 	void OnCollisionEnter2D(Collision2D collision2D){	
-		// TODO: Only detects hits from below.
-		RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector3.down, 1f);
-		if (hit.collider != null) {
-			if (hit.collider.rigidbody2D != null) {
-				activePlatform = hit.collider.transform;
-			} else {
-				activePlatform = null;
+		foreach (ContactPoint2D contact in collision2D.contacts) {
+			if (contact.normal == Vector2.up) {
+				if (contact.collider.rigidbody2D != null) {
+					activePlatform = contact.collider.transform;
+				} else {
+					activePlatform = null;
+				}
+				grounded = true;
 			}
-			//Debug.Log (hit.collider.gameObject.name);
-			grounded = true;
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D collision) {
+		foreach (ContactPoint2D contact in collision.contacts) {
+			if( (contact.normal == Vector2.right) ){
+				Vector2 playerSpeed = rigidbody2D.velocity;
+				playerSpeed.x = 0;
+				wallLeft = true;
+				rigidbody2D.velocity = playerSpeed;
+			} else if (contact.normal == (Vector2.right*-1)){
+				Vector2 playerSpeed = rigidbody2D.velocity;
+				playerSpeed.x = 0;
+				wallRight = true;
+				rigidbody2D.velocity = playerSpeed;
+			}
 		}
 	}
 
 	// There is no active platform on collision exit
 	void OnCollisionExit2D(Collision2D collision2D){
+		wallLeft = false;
+		wallRight = false;
 		activePlatform = null;
 	}
 
@@ -85,25 +116,14 @@ public class PlayerScript : MonoBehaviour {
 			// Get speed in correct direction
 			Vector2 tempSpeed =  new Vector2 (Input.GetAxisRaw ("Horizontal") * MaxSpeed, rigidbody2D.velocity.y);
 
-			// Flip sprite if direction changes
-			/*
-			if( transform.localScale.x < 0 && Input.GetAxisRaw ("Horizontal") > 0 ||
-			   	transform.localScale.x > 0 && Input.GetAxisRaw ("Horizontal") < 0) 
-			{	
-				tempScale.x *= -1;
-				transform.localScale = tempScale;
-			}
-			*/
+			// Move player horizontally only if it is not blocked by a wall 
+			//RaycastHit2D hitRight = Physics2D.Raycast (transform.position, Vector3.right, .5f);
+			//RaycastHit2D hitLeft = Physics2D.Raycast (transform.position, Vector3.left, .5f);
 
-			// Move player horizontally if it is not blocked by a wall
-			RaycastHit2D hitRight = Physics2D.Raycast (transform.position, Vector3.right, .5f);
-			RaycastHit2D hitLeft = Physics2D.Raycast (transform.position, Vector3.left, .5f);
-			if(hitRight.collider == null && tempSpeed.x > 0){
+			if(!wallRight && !wallLeft){
 				rigidbody2D.velocity = tempSpeed;
 			}
-			if(hitLeft.collider == null && tempSpeed.x < 0){
-				rigidbody2D.velocity = tempSpeed;
-			}
+
 		}
 	}
 	void jump(){
