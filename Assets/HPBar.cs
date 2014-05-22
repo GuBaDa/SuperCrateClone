c8e7e3a24099375801da21a93b46ef0cf9ec70a9
SpidersBro <barend.poot@gmail.com>
2014-05-22 12:48:26 +0200

    HP bar.
    Added HP simple HP bar and HP bar prefab. Also some cleaning up.

---
+++ Assets/HPBar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HPBar : MonoBehaviour {
+	private PlayerScript player;
+	private LineRenderer lineRenderer;
+	// Use this for initialization
+	void Start () {
+		player = transform.parent.GetComponent<PlayerScript> ();
+		//LineRenderer lineRenderer = GetComponent<LineRenderer>();
+		//lineRenderer.SetVertexCount( 1 );
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lineRenderer = GetComponent<LineRenderer>();
+
+		Vector3 pos = new Vector3(player.Health/100f,0,0);
+		lineRenderer.SetPosition(0, pos);
+	}
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -156,7 +156,6 @@ public class PlayerScript : MonoBehaviour {
 			grounded = false;
 			jumped = true;
 			dustCast ();
-			Debug.Log(Health);
 		}
 	}
 	void doubleJump(){
