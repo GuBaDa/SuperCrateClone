21e9af6673eb01f706473e58d93379a4361953cb
guusmost <guusmostart@gmail.com>
2014-05-25 15:07:41 +0200

    HPbar update + dust update + bullet sprite + cleanup

--- Assets/Scripts/HPBar.cs
+++ Assets/Scripts/HPBar.cs
@@ -5,19 +5,28 @@ public class HPBar : MonoBehaviour {
 
 	private PlayerScript player;
 	private LineRenderer lineRenderer;
+
+	private Color c1;
+	private Color c2;
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+		c1 = Color.red;
+		c2 = Color.green;
+
 		player = transform.parent.GetComponent<PlayerScript> ();
 		//LineRenderer lineRenderer = GetComponent<LineRenderer>();
 		//lineRenderer.SetVertexCount( 1 );
-
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		lineRenderer = GetComponent<LineRenderer>();
-
-		Vector3 pos = new Vector3(player.Health/100f,0,0);
+		float health = player.Health / 100f;
+		Vector3 pos = new Vector3(health,0,0);
+		Color newColor = new Color(1-(c1.r * health), c2.g * health * 2, 0, 1); 
+		lineRenderer.SetColors (newColor, newColor); 
 		lineRenderer.SetPosition(0, pos);
+
 	}
 }
