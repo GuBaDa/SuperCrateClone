4af15f7da8ba1567bbb091a1bb14132ea595b082
davidalpha <david.alpha@gmail.com>
2014-05-29 16:44:22 +0200

    Bouncy projectiles klaar

--- Assets/Scripts/ProjectileBouncy.cs
+++ Assets/Scripts/ProjectileBouncy.cs
@@ -3,24 +3,30 @@ using System.Collections;
 
 public class ProjectileBouncy: MonoBehaviour {
 
+	private int bounceCount;
+	public int bounceCountAmount;
+
 	// Use this for initialization
 	void Start () {
-		
+		bounceCount = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-				if (transform.rigidbody2D.velocity.x == 0) {
+		// amount of bounces before being destroyed
+				if (bounceCount == bounceCountAmount) {
 						Destroy (gameObject);
 				}
 	}
 
-	void OnCollisionEnter2D(Collision2D coll){
+		void OnCollisionEnter2D(Collision2D coll){
+		bounceCount += 1;
 		// For players
 		GameObject enemy = coll.gameObject;
 		if (enemy.tag == "Player") {
 			PlayerScript playerScript = enemy.GetComponent<PlayerScript>();
 			playerScript.Health -= 10.0f;
+			Destroy (gameObject);
 		} else if (enemy.tag == "Mob") {
 			MobScript mobScript = enemy.GetComponent<MobScript>();
 			mobScript.Health -= 10.0f;
