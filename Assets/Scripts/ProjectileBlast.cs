4ab1da11e310b8ff76ea96cdce055308124dfb6b
guusmost <guusmostart@gmail.com>
2014-05-31 06:06:38 +0200

    SkullGranate + blast + particles

---
+++ Assets/Scripts/ProjectileBlast.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileBlast : MonoBehaviour {
+
+	public float flashTime;
+	
+	// Update is called once per frame
+	void Awake () {
+		Invoke("DestroySelf", flashTime);
+	}
+
+
+	void OnCollisionEnter2D(Collision2D coll){
+		// For players
+		GameObject hit = coll.gameObject;
+		if (hit.tag == "Player") {
+			PlayerScript playerScript = hit.GetComponent<PlayerScript>();
+			playerScript.Health -= 10.0f;
+			Destroy (gameObject);
+		} else if (hit.tag == "Mob") {
+			MobScript mobScript = hit.GetComponent<MobScript>();
+			mobScript.Health -= 10.0f;
+		}
+	}
+
+	void DestroySelf (){
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/ProjectileGranate.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileGranate : MonoBehaviour {
+
+	public float detonateTime;
+	public GameObject blastPrefab;
+
+	public GameObject particleObject;
+
+	public float blastRange;
+	public int numberOfBlasts;
+
+	private bool loop; 
+	private ParticleSystem particles;
+
+	void Awake(){
+		//StartCoroutine (BlastSpawner(0.1f));
+		loop = false;
+		particles = particleObject.particleSystem;
+	}
+
+	void Update(){
+		detonateTime -= Time.deltaTime;
+
+		if (detonateTime < 0 && !loop){
+			particles.Play();
+			renderer.enabled = false;
+			StartCoroutine (BlastSpawner(0.01f));
+		}
+	}
+
+	IEnumerator BlastSpawner(float time) { 
+		loop = true;
+		Debug.Log ("IENUMERATOR");
+
+
+		for(int i=0; i<numberOfBlasts; i++) {
+			SpawnBlast(); 
+			if (i == numberOfBlasts -1){
+				particles.Stop();
+				yield return new WaitForSeconds(2f);
+				Destroy (gameObject);
+			}
+			yield return new WaitForSeconds(time);
+		}
+	}
+
+
+	void SpawnBlast(){
+		float randomX = Random.Range (0, blastRange *2) - blastRange; 
+		float randomY = Random.Range (0, blastRange *2) - blastRange;
+		Vector2 pos = new Vector2 (transform.position.x + randomX, transform.position.y + randomY);
+		GameObject pPrefab = (GameObject) Instantiate (blastPrefab, pos, Quaternion.identity);
+	}
+}
