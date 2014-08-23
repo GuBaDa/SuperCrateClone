13419f90581af0e491f02bdf4d020267e989180e
guusmost <guusmostart@gmail.com>
2014-08-23 22:30:19 +0200

    small up

--- Assets/Scripts/MovePlatforms.cs
+++ Assets/Scripts/MovePlatforms.cs
@@ -1,17 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 public class MovePlatforms : MonoBehaviour {
 	// This is a general script for moving platforms, Speed and Direction are public vars.
-	
 	public float moveSpeed;
 	public bool horizontal;
 	public float tilesToMove;
-
 	private float delay = 20;
 	private Vector3 startPosition;
 	private Vector2 moveForce;
-	
 	// Use this for initialization
 	void Start () {
 		if (!horizontal) {
@@ -22,20 +18,18 @@ public class MovePlatforms : MonoBehaviour {
 		}
 		startPosition = transform.position;
 	}
-	
 	// Update is called once per frame
 	void Update () {
-
 		if (delay > 20){
 			if (!horizontal) {
-				if (transform.position.y > startPosition.y + tilesToMove || 
+				if (transform.position.y > startPosition.y + tilesToMove ||
 				    transform.position.y < startPosition.y - tilesToMove){
 					moveForce.y *= -1;
 					delay = 0;
 				}
 			}
 			else {
-				if (transform.position.x > startPosition.x + tilesToMove || 
+				if (transform.position.x > startPosition.x + tilesToMove ||
 				    transform.position.x < startPosition.x - tilesToMove){
 					moveForce.x *= -1;
 					delay = 0;
@@ -44,6 +38,5 @@ public class MovePlatforms : MonoBehaviour {
 		}
 		transform.rigidbody2D.velocity = Time.deltaTime * moveForce;
 		delay++;
-
 	}
 }
