e25eb95547cb19a789f2f009a1c747e4e31c896e
SpidersBro <barendez@gmail.com>
2014-08-24 00:20:47 +0200

    Bullets kill tiles

---
+++ Assets/Scripts/TileMapColliderScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMapColliderScript : MonoBehaviour {
+
+	//[HideInInspector]
+
+	TileMapDestroyScript tileMap;
+
+	// Use this for initialization
+	void Start () {
+		tileMap = TileMapDestroyScript.FindObjectOfType<TileMapDestroyScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D coll) {
+		if(coll.collider.gameObject.tag == "Projectile"){
+			tileMap.projectileCollision = coll;
+		}
+	}
+}
---
+++ Assets/Scripts/TileMapDestroyScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMapDestroyScript : MonoBehaviour {
+	
+	private tk2dTileMap tilemap;
+	//private PlayerScript playerScript;
+	//private TileMapColliderScript tileMapCollider;
+
+	public Collision2D projectileCollision;
+	
+	// Use this for initialization
+	void Start () {
+		tilemap = GetComponent<tk2dTileMap>();
+		//tileMapCollider = GetComponent<TileMapColliderScript>();
+		//tileMapCollider = TileMapColliderScript.FindObjectOfType<TileMapColliderScript>
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(projectileCollision != null){
+			Debug.Log("Projectile hit the wall");
+			foreach ( ContactPoint2D contactpoint in projectileCollision.contacts){
+				Debug.Log(contactpoint.point);
+				tilemap.ClearTile((int)contactpoint.point.x,(int)contactpoint.point.y,3);
+			}
+			projectileCollision = null;
+			tilemap.Build();
+		}
+	}
+}
