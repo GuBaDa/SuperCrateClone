99284db32c7040ba345db2deed0375018d2a573c
SpidersBro <barendez@gmail.com>
2014-08-25 21:54:02 +0200

    NWES tiles set.

--- Assets/Scripts/TileMapDestroyScript.cs
+++ Assets/Scripts/TileMapDestroyScript.cs
@@ -41,6 +41,8 @@ public class TileMapDestroyScript : MonoBehaviour {
 				Debug.Log(tileId);
 
 				tilemap.Layers[3].ClearTile(xPos,yPos);
+				StartCoroutine (setNWESTiles(xPos,yPos));
+
 				tilemap.Build();
 
 				//Debug.Log(tileId2);
@@ -56,9 +58,68 @@ public class TileMapDestroyScript : MonoBehaviour {
 
 
 
+
+
 			}
 			projectileCollision = null;
 			//tilemap.Build();
 		}
 	}
+
+	
+	/// <summary>
+	/// receives coordinates from destroyed tile,
+	/// Checks tileIds from n,w,e,s directions and adds the appropriate binary string to them.
+	/// Sets the four n,w,e,s tiles to their new tileIds.
+	/// </summary>
+	/// <param name="str1">Str1.</param>
+	/// <param name="str2">Str2.</param>
+	/// 
+	IEnumerator setNWESTiles(int xPos, int yPos){
+		
+		// First remember the positions of the tiles N,W,E,S.
+
+		int north = tilemap.Layers[3].GetTile(xPos,yPos+1);
+		int west = tilemap.Layers[3].GetTile(xPos-1,yPos);
+		int east = tilemap.Layers[3].GetTile(xPos+1,yPos);
+		int south = tilemap.Layers[3].GetTile(xPos,yPos-1);
+
+
+		if(north >= 0 ){
+			tilemap.Layers[6].SetTile(xPos,yPos+1,convertAddBinary(north,1));
+		}
+
+		if (west >= 0 ) {
+			tilemap.Layers[6].SetTile(xPos-1,yPos,convertAddBinary(west,2));
+		}
+
+		if (east >= 0 ) {
+			tilemap.Layers[6].SetTile(xPos+1,yPos,convertAddBinary(east,4));
+		}
+		if (south >= 0 ) {
+			tilemap.Layers[6].SetTile(xPos,yPos-1,convertAddBinary(south,8));
+		}
+
+		yield return null;
+
+	}
+
+	int convertAddBinary(int tileID, int edgeDirection){
+
+		string s1 = System.Convert.ToString(tileID, 2); 
+		char[] charArray1 = s1.PadLeft(4, '0').ToCharArray();
+
+		string s2 = System.Convert.ToString(edgeDirection, 2); 
+		char[] charArray2 = s2.PadLeft(4, '0').ToCharArray();
+
+		for(int i = 0; i < charArray1.Length; i++ ){
+			if (charArray2[i] == '1' ){
+				charArray1[i] = '1';
+			}
+		}
+		string s = new string(charArray2);
+		int tileIDNew = System.Convert.ToInt32(s, 2);
+		return tileIDNew;
+	}
+	
 }
