8051d209080e1944c22eb38af3efced7f5d15e25
guusmost <guusmostart@gmail.com>
2014-08-26 14:49:15 +0200

    destruction update

--- Assets/Scripts/TileMapDestroyScript.cs
+++ Assets/Scripts/TileMapDestroyScript.cs
@@ -66,25 +66,82 @@ public class TileMapDestroyScript : MonoBehaviour {
 		int east = tilemap.Layers[6].GetTile(xPos+1,yPos);
 		int west = tilemap.Layers[6].GetTile(xPos-1,yPos);
 		int south = tilemap.Layers[6].GetTile(xPos,yPos-1);
+		//Get Tile ID's on layer 3 to check if tile is present, so cleared tiles can be skipped
+		int _N = tilemap.Layers[3].GetTile(xPos,yPos+1);
+		int _E = tilemap.Layers[3].GetTile(xPos+1,yPos);
+		int _W = tilemap.Layers[3].GetTile(xPos-1,yPos);
+		int _S = tilemap.Layers[3].GetTile(xPos,yPos-1);
 
 
+		//get diagonal Tile ID's 
+		int NW = tilemap.Layers[5].GetTile(xPos-1,yPos+1)-16;
+		int NE = tilemap.Layers[5].GetTile(xPos+1,yPos+1)-16;
+		int SE = tilemap.Layers[5].GetTile(xPos+1,yPos-1)-16;
+		int SW = tilemap.Layers[5].GetTile(xPos-1,yPos-1)-16;
 
-		if(north >= 0 ){
+		//Debug.Log ("NW: " + NW + "NE: " + NE + "SE: " + SE + "SW: " + SE);
+		//Get Tile ID's on layer 3 to check if tile is present, so cleared tiles can be skipped
+		int _NW = tilemap.Layers[3].GetTile(xPos-1,yPos+1);
+		int _NE = tilemap.Layers[3].GetTile(xPos+1,yPos+1);
+		int _SE = tilemap.Layers[3].GetTile(xPos+1,yPos-1);
+		int _SW = tilemap.Layers[3].GetTile(xPos-1,yPos-1);
 
+
+		if(_N >= 0 ){
+			if (north <0){
+				north = 0;
+			}
 			tilemap.Layers[6].SetTile(xPos,yPos+1,convertAddBinary(north,8));
 		}
 
-		if (east >= 0) {
+		if (_E >= 0) {
+			if (east <0){
+				east = 0;
+			}
 			tilemap.Layers[6].SetTile(xPos+1,yPos,convertAddBinary(east,4));
 		}
 
-		if(west >= 0 ){
+		if(_W >= 0 ){
+			if (west <0){
+				west = 0;
+			}
 			tilemap.Layers[6].SetTile(xPos-1,yPos,convertAddBinary(west,2));
 		}
 
-		if(south >= 0 ) {
+		if(_S >= 0 ) {
+			if (south <0){
+				south = 0;
+			}
 			tilemap.Layers[6].SetTile(xPos,yPos-1,convertAddBinary(south,1));
 		}
+
+
+		//diagonals
+		if (_NW >= 0 ){
+			if (NW <0){
+				NW = 0;
+			}
+			tilemap.Layers[5].SetTile(xPos-1,yPos+1, (convertAddBinary (NW,8) + 16 ));
+		}
+		if (_NE >= 0 ){
+			if (NE <0){
+				NE = 0;
+			}
+			tilemap.Layers[5].SetTile(xPos+1,yPos+1, (convertAddBinary (NE,4) + 16 ));
+		}
+		if (_SE >= 0 ){
+			if (SE <0){
+				SE = 0;
+			}
+			tilemap.Layers[5].SetTile(xPos+1,yPos-1, (convertAddBinary (SE,1) + 16 ));
+		}
+		if (_SW >= 0 ){
+			if (SW <0){
+				SW = 0;
+			}
+			tilemap.Layers[5].SetTile(xPos-1,yPos-1, (convertAddBinary (SW,2) + 16 ));
+		}
+
 
 		yield return null;
 
