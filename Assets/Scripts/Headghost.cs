09a2c65df61a85e0fd25e1db6c47ffc123e1a1a1
davidalpha <david.alpha@gmail.com>
2014-09-03 16:35:16 +0200

    Updated prefab en graphics HeadGhost

--- Assets/Scripts/Headghost.cs
+++ Assets/Scripts/Headghost.cs
@@ -2,192 +2,53 @@
 using System.Collections;
 
 public class Headghost : MonoBehaviour {
-
-public float speed;	
-public float agroSpeed;
-public float agroDistance;
-
-
-private float timerGlobal;
-private float timerWandering = 0;
-private float wanderingPauseTime = 0;
-private float timerAgro = 0;
-private float agroPauseTime = 0;
-
-
-private int direction = 1;
-private float rayDistance = 6;
-
-private GameObject target;
-
-private enum Action { 
-	Wandering,
-	Agro,
-}
-
-private Action action;
-
-// Use this for initialization
-void Awake (){
-}
-
-// Update is called once per frame
-void Update () {
-	Timer ();
-	//RaycastCollision ();
-	//check for Agro
-	if (checkForAgro()){
-		action = Action.Agro;
-	}
-	else{
-		action = Action.Wandering;
-	}
+	
+	Animator anim;
+	
+	public float force;
+	public float swimTimeFactor;
+	
+	private float time;
 	
 	
-	caseSwitcher ();
-	RaycastCollision ();
-	//Debug.Log (timerAgro);
-}
-
-
-
-void caseSwitcher(){
-	switch (action){
-	case Action.Wandering :
-		doWandering ();
-		//Debug.Log ("Wandering");
-		break;
-	case Action.Agro :
-		doAgro (target);
-		//Debug.Log ("Agro");
-		break;
+	// Use this for initialization
+	void Start (){
+		
+		anim = GetComponent<Animator> ();
+		
+		time = Random.Range (swimTimeFactor, swimTimeFactor * 2);
+		InvokeRepeating("swim", 0, time);
 	}
-}
-
-
-void doWandering (){
-	//Wandering routine
 	
-	if (timerWandering > 0){
-		//charge
-			rigidbody2D.velocity = new Vector2 (speed * direction, speed * direction);
-	}
-	else{
-		if (timerWandering > -wanderingPauseTime){
-			//pause wandering
-			rigidbody2D.velocity = new Vector2 (0, 0);
-			if (timerGlobal <= 0){
-				//switch direction while in pause
-				direction *= -1;
-				timerGlobal = Random.Range (1f, 3f);
-			}
-		}
-		else {
-			//set new value timerAgro and agroTimerPause
-			timerWandering = Random.Range (1f, 3f);
-			wanderingPauseTime = Random.Range (0.5f, 2f);
-		}
+	// Update is called once per frame
+	void Update () {
 		
 	}
-}
-
-
-void doAgro (GameObject target){
-	//Agro routine
 	
-	agroSpeed = Mathf.Abs (agroSpeed);
-	if (transform.position.x + 0.2 > target.transform.position.x) {
-		agroSpeed = -agroSpeed;
-	}
-	if (timerAgro > 0){
-		//charge
-		rigidbody2D.velocity = new Vector2 (agroSpeed, 0);
-	}
-	else{
-		if (timerAgro > -agroPauseTime){
-			//pause charge
-			rigidbody2D.velocity = new Vector2 (0, 0);
+	void OnCollisionEnter2D(){
+		float dir = 1;
+		if (rigidbody2D.velocity.y > 0){
+			dir = -1;
 		}
-		else {
-			//set new value timerAgro and agroTimerPause
-			timerAgro = Random.Range (0.2f, 0.5f);
-			agroPauseTime = Random.Range (0f, 0.3f);
-		}
-	}
-}
-
-
-void RaycastCollision (){
-	//Get collision left or right and change directiopn and reset timerGlobal
-	
-	RaycastHit2D leftRay = Physics2D.Raycast (new Vector2(transform.position.x, transform.position.y + 1), Vector2.right, -rayDistance); 
-	RaycastHit2D rightRay = Physics2D.Raycast (new Vector2(transform.position.x, transform.position.y + 1), Vector2.right, rayDistance); 
-	if (leftRay.collider != null && leftRay.rigidbody == null){
-		direction = 1;
-		timerGlobal = Random.Range (1f, 3f);
-	}
-	if (rightRay.collider != null && leftRay.rigidbody == null){
-		direction = -1;
-		timerGlobal = Random.Range (1f, 3f);
+		Vector2 forceVector = new Vector2 (Random.Range(force/-3, dir * force/3), force);
+		rigidbody2D.AddForce (forceVector);
+		anim.SetBool("animIdle", false);
+		anim.SetBool("animSwim", true);
 	}
 	
+	void swim (){
+		Vector2 forceVector = new Vector2 (Random.Range(force/-3, force/3), force);
+		rigidbody2D.AddForce (forceVector);
+		time = Random.Range (swimTimeFactor, swimTimeFactor * 2);
+		anim.SetBool("animIdle", false);
+		anim.SetBool("animSwim", true);
+		
+	}
+	
+	public void swimEnd(){
+		anim.SetBool("animIdle", true);
+		anim.SetBool("animSwim", false);
+		
+	}
 	
 }
-
-
-
-private bool checkForAgro (){
-	//Debug.Log ("CHECK foragro");
-	GameObject[] playersAvailable = GameObject.FindGameObjectsWithTag ("Player");
-	if (playersAvailable.Length != 0){
-		target = findClosestTarget ();
-		if (Mathf.Abs(target.transform.position.x - transform.position.x) < agroDistance &&
-		    Mathf.Abs(target.transform.position.y - transform.position.y) < 3)
-		{
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	else {
-		return false;
-	}
-}
-
-
-GameObject findClosestTarget(){
-	//Find and return closest Player
-	
-	GameObject[] targets;
-	targets = GameObject.FindGameObjectsWithTag ("Player");
-	GameObject closestTarget = null;
-	float distance = Mathf.Infinity;
-	Vector3 position = transform.position;
-	foreach (GameObject tar in targets) {
-		Vector3 diff = tar.transform.position - position;
-		float curDistance = diff.sqrMagnitude;
-		if (curDistance < distance) {
-			closestTarget = tar;
-			distance = curDistance;
-		}
-	}
-	return closestTarget;
-}
-
-
-void Timer() {
-	//Function for substracting timer per deltaTime
-	
-	if (timerGlobal > 0){
-		timerGlobal -= Time.deltaTime;
-	}
-	if (timerAgro > - agroPauseTime){
-		timerAgro -= Time.deltaTime;
-	}
-	if (timerWandering > - wanderingPauseTime){
-		timerWandering-= Time.deltaTime;
-	}
-}
-}
-
