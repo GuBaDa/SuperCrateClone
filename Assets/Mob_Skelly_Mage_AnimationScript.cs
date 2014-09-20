38dcc50de9541415fd5e36b4641c3980cdf787b0
SpidersBro <barendez@gmail.com>
2014-09-20 21:37:32 +0200

    Added Skelly Mage
    Skelly mage and some cleanup

---
+++ Assets/Mob_Skelly_Mage_AnimationScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mob_Skelly_Mage_AnimationScript : MonoBehaviour {
+
+	Animator anim;
+	// Use this for initialization
+	void Awake () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(checkForAgro()){
+			anim.SetBool ("animWalk", false);
+			anim.SetBool ("animIdle", false);
+			anim.SetBool ("animCast", true);
+		} else if(rigidbody2D.velocity.x != 0){
+			anim.SetBool ("animWalk", true);
+			anim.SetBool ("animIdle", false);
+			anim.SetBool ("animCast", false);
+		} else {
+			anim.SetBool ("animWalk", false);
+			anim.SetBool ("animIdle", true);
+			anim.SetBool ("animCast", false);
+		}
+	}
+
+	private bool checkForAgro (){
+		//Debug.Log ("CHECK foragro");
+		GameObject[] playersAvailable = GameObject.FindGameObjectsWithTag ("Player");
+		if (playersAvailable.Length != 0){
+			GameObject target = findClosestTarget ();
+			if (Mathf.Abs(target.transform.position.x - transform.position.x) < 3 &&
+			    Mathf.Abs(target.transform.position.y - transform.position.y) < 3)
+			{
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+
+	GameObject findClosestTarget(){
+		//Find and return closest Player
+		
+		GameObject[] targets;
+		targets = GameObject.FindGameObjectsWithTag ("Player");
+		GameObject closestTarget = null;
+		float distance = Mathf.Infinity;
+		Vector3 position = transform.position;
+		foreach (GameObject tar in targets) {
+			Vector3 diff = tar.transform.position - position;
+			float curDistance = diff.sqrMagnitude;
+			if (curDistance < distance) {
+				closestTarget = tar;
+				distance = curDistance;
+			}
+		}
+		return closestTarget;
+	}
+}
---
+++ Assets/Scripts/Mob_Skelly_Script.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mob_Skelly_Script : MonoBehaviour {
+
+	// Public vars
+	public float moveSpeed;
+
+	//Private vars
+	private bool grounded;
+	private bool casting;
+
+
+
+	// Use this for initialization
+	void Awake () {
+		casting = false;
+	}
+
+	void OnTriggerStay2D(){
+		grounded = true;
+	}
+
+	void OnTriggerExit2D(){
+		grounded = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(grounded && !casting) {
+			doWalk();
+		} else if (!casting) {
+			rigidbody2D.velocity = new Vector2(0,rigidbody2D.velocity.y);
+		}
+	}
+
+	void doWalk(){
+		casting = false;
+		rigidbody2D.velocity = new Vector2(moveSpeed,0);
+	}
+
+	void castSpell(){
+		casting = true;
+	}
+}
