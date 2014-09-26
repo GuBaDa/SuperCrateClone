73af8e815a98f402bf9c841360cf21dcc6e4b3b9
SpidersBro <barendez@gmail.com>
2014-09-26 15:17:21 +0200

    skellyfloat update 6
    now kinda floats i guess

---
+++ Assets/Mob_Skelly_Ghost_Script.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mob_Skelly_Ghost_Script : MonoBehaviour {
+
+	private float timeSinceFloat;
+	private float coolDown;
+
+	// Use this for initialization
+	void Awake () {
+		coolDown = .2f;
+		timeSinceFloat =0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeSinceFloat += Time.deltaTime;
+		if(timeSinceFloat > coolDown){
+			timeSinceFloat = 0;
+			doFloat();
+		}
+	}
+
+
+
+
+	void doFloat(){
+		rigidbody2D.AddForce(new Vector2(2f,1f),ForceMode2D.Impulse);
+
+	}
+}
